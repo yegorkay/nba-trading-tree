443c84398316fe2c2230516f08f6ff72ad1c43a5
Yegor Kariaguine <yegor.kariaguine@chefsplate.com>
2020-01-15 10:57:31 -0500

    made multi-team trades work

--- README.md
+++ README.md
@@ -1,14 +1,3 @@
-currently this only works if we have trades involving:
-
-- two teams
-- one player for many players
-
-it should work for:
-
-- multi-team
-- many players for many players
-
-try to implement soon:
-
-- showing the correct tradedFrom and tradedTo values if more than 1 player is traded to another team
-- i.e. Kawhi + 2 other player for George Hill
+something breaks when we search:
+- Antoine Walker
+- Kyrie Irving
--- src/services/tradeService.ts
+++ src/services/tradeService.ts
@@ -4,32 +4,62 @@ import { transactionSelector } from '../settings';
 import he from 'he';
 
 class TradeService {
-  private splitString(html: string, index: number): string[] | undefined {
-    const transaction = $(
+  /**
+  * Gets the team abbreviation found in a `data-attr` tag for the given HTML.
+  * @param html The HTML context.
+  * @param index Used as part of a CSS selector to find HTML where a trade occurs.
+  * @returns An array of HTML strings.
+  */
+  private splitHTML(html: string, index?: number): string[] {
+
+    /** If the index is not passed, we are reading already parsed HTML,
+     * and more likely an HTML string that was a part of a multi-team trade */
+    if (!index) return html.split('to the ');
+
+    const transactionHTML = $(
       `${transactionSelector}:nth-child(${index + 1})`,
       html
-    );
-    const transactionHTML = transaction.html();
+    ).html();
+
     if (transactionHTML) {
       const isMultiTrade = transactionHTML
         ?.toLowerCase()
         .includes('as part of');
-      /** Decode any HTML entities so we don't have false positives splitting a string on a semi-colon `(;)` */
+      /** Decode any HTML entities so we don't have false
+       * positives splitting a string on a semi-colon `(;)` */
       const decodedHTML = isMultiTrade
         ? he.decode(transactionHTML)
         : transactionHTML;
       const splitKey = isMultiTrade ? '; ' : 'to the ';
-      return decodedHTML?.split(splitKey);
+      return decodedHTML.split(splitKey);
+    } else {
+      return [];
     }
-    return undefined;
   }
-
+  /**
+   * Gets the team abbreviation found in a `data-attr` tag for the given HTML.
+   * @param context The HTML context in which we are finding the selected team in a string.
+   * @param to The team side which we are selected. Either `tradedFrom` or `tradedTo`.
+   * @returns The team abbreviation found in the HTML.
+   * @example
+   * getTeam('<a data-attr-to="PHI" /><a data-attr-from="CLE" />');
+   * // "CLE"
+   * getTeam('<a data-attr-to="PHI" /><a data-attr-from="CLE" />', true);
+   * // "PHI"
+   */
   private getTeam(context: string, to = false): string {
     const side = to ? 'to' : 'from';
     return $(`a[data-attr-${side}]`, context).attr(`data-attr-${side}`);
   }
-
-  getPlayersInSection(
+  /**
+   * Gets the players involved in a trade for one side of the trade.
+   * Either the players who were `tradedFrom` a team, or `tradedTo` a team.
+   * @param tradedFromCtx The side of the HTML where `traded from` occurs.
+   * @param tradedToCtx The side of the HTML where `traded to` occurs.
+   * @param swap An optional argument that swaps the teams involved.
+   * @returns An array of PlayerIDs.
+   */
+  private getPlayersInSection(
     tradedFromCtx: string,
     tradedToCtx: string,
     swap = false
@@ -51,37 +81,63 @@ class TradeService {
     });
     return data;
   }
+  /**
+   * Gets all players involved in a multi-team trade for a given date.
+   * @param dividedHTML The array of HTML trade strings.
+   * @returns An array of PlayerIDs.
+   */
+  private getMultiTradeData(dividedHTML: string[]): PlayerID[] {
+    const splitHTMLData: string[][] = [];
+    const playerData: PlayerID[] = [];
 
-  public getPlayerData(html: string, index: number) {
-    const dividedHTML = this.splitString(html, index);
-    if (dividedHTML) {
-      const normalTrade = dividedHTML.length === 2;
+    dividedHTML.forEach((html) => {
+      const splitHTML = this.splitHTML(html);
+      splitHTMLData.push(splitHTML);
+    });
+    splitHTMLData.forEach((html: string[]) => {
+      const [tradedFrom, tradedTo] = html;
+      const tradeData = [
+        ...this.getPlayersInSection(tradedFrom, tradedTo),
+        ...this.getPlayersInSection(tradedFrom, tradedTo, true)
+      ];
+      playerData.push(...tradeData);
+    })
+    return playerData;
+  }
+  /**
+   * Gets all players involved in a trade for a given date.
+   * @param html The HTML page of the player we have searched for.
+   * @param index The index is used as an argument for `splitHTML(html, index)`
+   * which is used to find HTML where a trade occurs.
+   * @returns An array of PlayerIDs.
+   */
+  private getPlayerData(html: string, index: number): PlayerID[] {
+    const splitHTML = this.splitHTML(html, index);
+    if (splitHTML.length > 0) {
+      const normalTrade = splitHTML.length === 2;
       if (normalTrade) {
-        const [tradedFrom, tradedTo] = dividedHTML;
+        const [tradedFrom, tradedTo] = splitHTML;
         const tradeData = [
           ...this.getPlayersInSection(tradedFrom, tradedTo),
           ...this.getPlayersInSection(tradedFrom, tradedTo, true)
         ];
         return tradeData;
       } else {
-        dividedHTML.shift();
-        console.log(`${dividedHTML[0]}`);
-        // console.log(
-        //   this.getTeam(dividedHTML[0]),
-        //   this.getTeam(dividedHTML[0], true)
-        // );
-        // dividedHTML.forEach((html) => {
-        //   console.log({
-        //     tradedFrom: this.getTeam(html),
-        //     tradedTo: this.getTeam(html, true)
-        //   });
-        // });
+        /** Removing first element as we don't need
+         * the trade data for the player we are searching for */
+        splitHTML.shift();
+        const multiTradeData = this.getMultiTradeData(splitHTML);
+        return multiTradeData;
       }
     }
     return [];
   }
-
-  getAllPlayers(html: string, foundTradeIndices: number[]): PlayerID[][] {
+  /**
+   * Gets all players that were ever involved in a trade.
+   * @param html The HTML page of the player we have searched for.
+   * @returns An array of PlayerID arrays, where each array is a trade date found.
+   */
+  public getAllPlayers(html: string, foundTradeIndices: number[]): PlayerID[][] {
     let results: PlayerID[][] = [];
     for (let i = 0; i < foundTradeIndices.length; i++) {
       results.push(this.getPlayerData(html, foundTradeIndices[i]));

11090be70724bd5a554b90d235c0109b3cccfa1c
yegor.kariaguine <yegor.kariaguine@chefsplate.com>
2020-02-10 19:31:52 -0500

    cleaning up db code

--- src/db.ts
+++ src/db.ts
@@ -1,10 +1,6 @@
 import mongoose from 'mongoose';
-// // import dotenv from 'dotenv';
 import chalk from 'chalk';
 import { config } from './settings';
-// dotenv.config();
-
-// const dbURL = process.env.DB || '';
 
 mongoose.connect(config.db, {
   useNewUrlParser: true,
